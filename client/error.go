4146eaca7b6a28066a008eb6bfcae5e314b88265
Winston Howes <winstonhowes@gmail.com>
2024-05-13 14:26:41 -0700

    handle errors properly

--- client/error.go
+++ client/error.go
@@ -2,21 +2,17 @@
 package client
 
 import (
-	"encoding/json"
 	"fmt"
 )
 
 // WarrantedError provides information about an unsuccessful request.
 type WarrantedError struct {
-	Code     int                    `json:"code"`
-	Details  map[string]interface{} `json:"details"`
-	Message  string                 `json:"message"`
-	MoreInfo string                 `json:"more_info"`
-	Status   int                    `json:"status"`
+	ErrorCode string `json:"errorCode"`
+	IsError   bool   `json:"error"`
+	Message   string `json:"message"`
 }
 
 func (err *WarrantedError) Error() string {
-	detailsJSON, _ := json.Marshal(err.Details)
-	return fmt.Sprintf("Status: %d - ApiError %d: %s (%s) More info: %s",
-		err.Status, err.Code, err.Message, detailsJSON, err.MoreInfo)
+	return fmt.Sprintf("Status: Error - ApiError %s: %s ",
+		err.ErrorCode, err.Message)
 }
